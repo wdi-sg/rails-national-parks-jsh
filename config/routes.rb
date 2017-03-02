65e9c83dceec33cab47dcc221f2bb8578971c79a
hansheng quek <hsquek06@gmail.com>
2017-03-02 19:39:07 +0800

    Working version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'parks/index'
+  # get 'parks/:id', to: 'parks/show'
+  # get 'welcome/index'
 
   resources :parks
-root 'welcome#index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
