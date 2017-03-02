c81e03f754f6687d65db39ca4119d73d519d8ddb
Sruti Keerti <skeerti2@gmail.com>
2017-03-02 19:12:24 +0800

    controllers update

---
+++ app/controllers/park_controller.rb
@@ -0,0 +1,9 @@
+class ParkController < ApplicationController
+  def index
+    @parks = Park.all
+  end
+
+  def show
+    @park=Park.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'park/index'
+  get 'park/:id', to:'park/show'
   get 'welcome/index'
+
+  resources :parks
 
 root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
